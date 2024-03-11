7dd6f5f1eef2ea467b496629288b79a372d60bfe
Vladyslav Kuchma <vladyslav.kuchma@nixs.com>
2024-03-11 15:38:49 +0200

    Create comments markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddCommentComponent } from './components/add-comment/add-comment.component';
+import { NgOptimizedImage } from '@angular/common';
+import { UserAvatarComponent } from './components/user-avatar/user-avatar.component';
+import { CommentBoxComponent } from './components/comment-box/comment-box.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { CommentActionsComponent } from './components/comment/comment-actions/comment-actions.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    AddCommentComponent,
+    UserAvatarComponent,
+    CommentBoxComponent,
+    CommentComponent,
+    CommentActionsComponent,
+  ],
+  imports: [BrowserModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/comment/comment-actions/comment-actions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-actions',
+  templateUrl: './comment-actions.component.html',
+  styleUrls: ['./comment-actions.component.scss']
+})
+export class CommentActionsComponent {
+
+}
