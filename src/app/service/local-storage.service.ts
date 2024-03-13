eef20b168a4ccbbdf73c5dfbf0b3c5e2d6fff9ec
Vladyslav Kuchma <vladyslav.kuchma@nixs.com>
2024-03-13 15:33:49 +0200

    Add async

--- src/app/service/local-storage.service.ts
+++ src/app/service/local-storage.service.ts
@@ -27,43 +27,64 @@ export class LocalStorageService {
     this.refreshComments();
   }
 
-  addComment(comment: NewCommentProps): void {
-    const existingComments = this.getComments();
-
-    const newComment = {
-      ...defaultComment,
-      ...comment,
-      id: String(Date.now()),
-    };
-
-    localStorage.setItem(
-      this.commentsKey,
-      JSON.stringify([...existingComments, newComment])
-    );
-
-    this.refreshComments();
-  }
-
-  editComment(updatedComment: NewCommentProps, id: string): void {
-    const existingComments = this.getComments();
-    const updatedComments = existingComments.map(comment => {
-      if (comment.id === id) {
-        comment = { ...comment, ...updatedComment };
-      }
-
-      return comment;
+  private simulateRequest(): Promise<void> {
+    return new Promise<void>(resolve => {
+      setTimeout(resolve, 500);
     });
-    localStorage.setItem(this.commentsKey, JSON.stringify(updatedComments));
-    this.refreshComments();
   }
 
-  removeComment(id: string): void {
-    const existingComments = this.getComments();
-    const updatedComments = existingComments.filter(
-      comment => comment.id !== id
-    );
-    localStorage.setItem(this.commentsKey, JSON.stringify(updatedComments));
-    this.refreshComments();
+  private getComments(): Comment[] {
+    const existingComments = localStorage.getItem(this.commentsKey);
+    return existingComments ? JSON.parse(existingComments) : [];
+  }
+
+  private refreshComments(): void {
+    const comments = this.getComments();
+    this.commentsSubject.next(comments);
+  }
+
+  addComment(comment: NewCommentProps): Promise<void> {
+    return this.simulateRequest().then(() => {
+      const existingComments = this.getComments();
+      const newComment = {
+        ...defaultComment,
+        ...comment,
+        id: String(Date.now()),
+      };
+      localStorage.setItem(
+        this.commentsKey,
+        JSON.stringify([...existingComments, newComment])
+      );
+      this.refreshComments();
+    });
+  }
+
+  editComment(updatedComment: NewCommentProps, id: string): Promise<void> {
+    return this.simulateRequest().then(() => {
+      const existingComments = this.getComments();
+      const updatedComments = existingComments.map(comment => {
+        if (comment.id === id) {
+          comment = { ...comment, ...updatedComment };
+        }
+
+        return comment;
+      });
+
+      localStorage.setItem(this.commentsKey, JSON.stringify(updatedComments));
+      this.refreshComments();
+    });
+  }
+
+  removeComment(id: string): Promise<void> {
+    return this.simulateRequest().then(() => {
+      const existingComments = this.getComments();
+      const updatedComments = existingComments.filter(
+        comment => comment.id !== id
+      );
+
+      localStorage.setItem(this.commentsKey, JSON.stringify(updatedComments));
+      this.refreshComments();
+    });
   }
 
   persistComment(persistedComment: PersistedCommentProps, id?: string): void {
@@ -96,16 +117,6 @@ export class LocalStorageService {
 
     const existingComments = this.getComments();
     return existingComments.find(comment => comment.id === id);
-  }
-
-  private getComments(): Comment[] {
-    const existingComments = localStorage.getItem(this.commentsKey);
-    return existingComments ? JSON.parse(existingComments) : [];
-  }
-
-  private refreshComments(): void {
-    const comments = this.getComments();
-    this.commentsSubject.next(comments);
   }
 
   getFilteredComments(selectedTag: string): Observable<Comment[]> {
